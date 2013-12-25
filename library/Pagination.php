a6443b3f3058edf2d2defbb7f143ee9c6adf12a0
Chen.Zhidong <njutczd@gmail.com>
2013-12-25 17:07:35 +0800

    Pagination修改数组索引名，去掉page前缀，assign到view中变量

--- library/Pagination.php
+++ library/Pagination.php
@@ -6,7 +6,7 @@
 
 class Pagination {
 	const TYPE_PARAM=1; /** /1 */
-	const TYPE_ENTITY=2; /** &page=1 */
+	const TYPE_ENTITY=2; /** &p=1 */
 	const TYPE_DETAIL=3; /** /page/1 */
 
 	/**
@@ -22,37 +22,35 @@ class Pagination {
 	 */
 	public static function splite($type, $view, $prefix, $total, $current, $count, $gap = 3) {
 		$current = ($current > 0 ? $current : 1);
-		$pagination_info['page_type'] = $type;
-		$pagination_info['page_total'] = $total;
-		$pagination_info['page_current'] = $current;
-		$pagination_info['page_prev'] = $current - 1;
-		$pagination_info['page_next'] = $current + 1;
-		$pagination_info['page_per'] = $count;
-		$pagination_info['page_gap'] = 3;
-
-		/*
-		$prefix=Tools::getHttpHost(true).substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], '?')).'?';
-		$queries=array_merge($_POST,$_GET);
-		foreach ($queries as $key=>$query){
-			if($key!='p'){
-				$prefix.=$key.'='.$query.'&';
-			}
+		$pagination_info['type'] = $type;
+		$pagination_info['total'] = $total;
+		$pagination_info['current'] = $current;
+		$pagination_info['prev'] = $current - 1;
+		$pagination_info['next'] = $current + 1;
+		$pagination_info['per'] = $count;
+		$pagination_info['gap'] = 3;
+		if(!empty($_GET)){
+			if($type==Pagination::TYPE_ENTITY && isset($_GET['p']))
+				unset($_GET['p']);
+			$pagination_info['query']='?'.implode('&',$_GET);
 		}
-		*/
-		$pagination_info['page_prefix'] = $prefix;
+		else
+			$pagination_info['query']='';
+
+		$pagination_info['prefix'] = $prefix;
 
 		$page_count = (int) ceil($total / $count);
-		$pagination_info['page_count'] = $page_count;
+		$pagination_info['count'] = $page_count;
 
 		$page_start = (int) $current - $gap;
 		$page_start = ($page_start < 1 ? 1 : $page_start);
-		$pagination_info['page_start'] = $page_start;
+		$pagination_info['start'] = $page_start;
 
 		$page_stop = (int) $current + $gap;
 		$page_stop = ($page_stop > $page_count ? $page_count : $page_stop);
-		$pagination_info['page_stop'] = $page_stop;
+		$pagination_info['stop'] = $page_stop;
 
-		$view->assign('pagination_info', $pagination_info);
+		$view->assign('pager', $pagination_info);
 		return $pagination_info;
 	}
 }
